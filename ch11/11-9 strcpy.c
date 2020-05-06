ebf5888c80de54da7445356c89e3a3d45638a5ae
Leechanjun <64836547+Leechanjun@users.noreply.github.com>
2020-05-06 18:13:23 +0900

    Add files via upload

---
+++ ch11/11-9 strcpy.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char dest[80] = "Java";
+	char sourcep[80] = "C is language";
+
+	printf("%s\n", strcpy(dest, source));
+	printf("%s\n", strcpy(dest, "C#", 2));
+	printf("%s\n", strcpy(dest, "C#", 3));
+
+	return 0;
+}
